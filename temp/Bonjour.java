05093b4b2613d5eae181ac0bdae9339eece7af2a
dom <domilung@gmail.com>
2017-12-19 00:29:18 +0100

    add bonjour.java

---
+++ temp/Bonjour.java
@@ -0,0 +1,12 @@
+public class Bonjour {
+
+	// La methode main
+	public static void main (String[] args) {
+	
+		System.out.println("Bonjour tout le monde");
+	
+	} // Fin de ma méthode main 
+
+
+
+} // Fin de ma classe 
